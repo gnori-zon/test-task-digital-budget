5aa2103aa5c00383e7db7f05b49b2305adafdcab
gnori-zon <konasenkos@gmail.com>
2023-06-02 20:06:27 +0300

    - init project

---
+++ src/main/java/org/gnori/testtaskdigitalbudget/TestTaskDigitalBudgetApplication.java
@@ -0,0 +1,13 @@
+package org.gnori.testtaskdigitalbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskDigitalBudgetApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestTaskDigitalBudgetApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/gnori/testtaskdigitalbudget/TestTaskDigitalBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gnori.testtaskdigitalbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskDigitalBudgetApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
